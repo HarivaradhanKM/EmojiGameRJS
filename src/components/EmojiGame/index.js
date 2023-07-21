d52d442acb2db4f573e69e00fdce16a5810652c2
Harivaradhan <harivaradhankm@gmail.com>
2023-07-21 07:34:54 +0000

    EmojiGameRJS

---
+++ src/components/EmojiGame/index.js
@@ -0,0 +1,13 @@
+/* 
+Quick Tip 
+
+- Use the below function in the EmojiGame Component to shuffle the emojisList every time when an emoji is clicked.
+
+const shuffledEmojisList = () => {
+  const {emojisList} = this.props
+  return emojisList.sort(() => Math.random() - 0.5)
+}
+
+*/
+
+// Write your code here.
